a014480ecf93ee282d62fe078dc6de4ec9711bdd
prasanthkarthik25305 <22p31a0564@acet.ac.in>
2025-12-19 19:28:21 +0530

    newe port added to main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from app.routers.book_router import router as book_router
 from app.routers.member_router import router as member_router
 from app.routers.transaction_router import router as transaction_router
 
+import os
+
 app = FastAPI(title="Library Management System API")
 
 Base.metadata.create_all(bind=engine)
@@ -12,3 +14,14 @@ Base.metadata.create_all(bind=engine)
 app.include_router(book_router)
 app.include_router(member_router)
 app.include_router(transaction_router)
+
+# -------------------------------
+# Render / Production Entry Point
+# -------------------------------
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8000))
+    )
